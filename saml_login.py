b989f14f87f66104446e64455416f69b588b519e
Herwig bogaert <herwig.bogaert@viaa.be>
2020-08-26 10:10:13 +0200

    refactor and add comments

--- saml_login.py
+++ saml_login.py
@@ -32,11 +32,18 @@ def report_and_exit(message,channels):
 if __name__ == "__main__":
     # interpret first command line parameter as json object
     data = json.loads(sys.argv[1])
-    login, logout = data['params'].replace('\\','').split(',')
+    # The parameters are sp login url, sp logout url [,  application host ]
+    # The application host is usually the same host as the sp, therefore
+    # it defaults to the sp hostname.
+    params = data['params'].replace('\\','').split(',')
+    if len(params) == 2: params += [ data['host'] ]
+    login, logout, apphost = params
 
     timer = Timer()
     channels = []
 
+    # A Get request to the login url of the app should redirect the user to the
+    # IFP. The latter will respond with a login form.
     step = 'Redirect to IDP'
     r_app = requests.get('https://{}/{}'.format(data['host'],login))
     saml_redirect = (next(( r for r in r_app.history + [r_app]
@@ -56,34 +63,40 @@ if __name__ == "__main__":
         report_and_exit(message,channels)
     channels.append({'name': step, 'value': timer.get_elapsed()})
 
+    # We submit the filled in login form to the IDP
     step = 'IDP login'
     form_data = {
             'username': data['linuxloginusername'],
             'password': data['linuxloginpassword']
             }
     r_login = loginform.submit(form_data)
-    # This response always has status code 200, even when the login failed
-    # Succesfull login is iindicated by the presence of the cookie
+    # The IDP login response always has status code 200, even when the login failed
+    # Succesfull login is indicated by the presence of the cookie
     # 'SimpleSAMLAuthToken'
     auth_token = r_login.cookies.get('SimpleSAMLAuthToken', None)
     if not auth_token:
         channels.append({'name': step, 'error': 'IDP Login failed' })
         report_and_exit(message,channels)
     channels.append({'name':step, 'value': timer.get_elapsed()})
+    # The IDP response contains a form that needs to be submitted to the
+    # service provider. (In the # browser this is automated by a javascript script)
     saml_form = HTMLForm(r_login)
     saml_form.add_cookies(loginform.cookiejar)
 
     step = 'Application login'
     r_app = saml_form.submit()
-    # SAML login response always returns HTTP status 200
-    # But succesfull saml login results in  a redirect to the app
+    # succesfull saml post to the service provider assertion consumer url
+    # results in a redirect to an app session. This location must correspond
+    # to the apphost (which is usually the same as the sp host)
     redirected = (next(( r for r in r_app.history
-        if urlparse(r.headers['location']).netloc == data['host']), None))
+        if urlparse(r.headers['location']).netloc == apphost), None))
     if not redirected:
         channels.append({'name': step, 'error': 'IDP does not redirect back' })
         report_and_exit(message,channels)
     channels.append({'name': step, 'value': timer.get_elapsed()})
 
+    # A Get request on the logout url will redirect to the IDP for
+    # terminating the session there
     step = 'IDP Logout'
     r_logout = requests.get('https://{}/{}'.format(data['host'],logout),
             cookies=saml_form.cookiejar)
@@ -93,6 +106,8 @@ if __name__ == "__main__":
         report_and_exit(message,channels)
     channels.append({'name': step, 'value': timer.get_elapsed()})
 
+    # After logout on the IDP, the IDP will redirect to the logout consumer of
+    # the application.
     step = 'Application logout'
     saml_logout_form = HTMLForm(r_logout)
     saml_logout_form.add_cookies(saml_form.cookiejar)
